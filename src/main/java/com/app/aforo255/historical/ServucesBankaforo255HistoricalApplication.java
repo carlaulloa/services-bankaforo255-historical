c81f1ed1dec77a617479efa3772a477f24a31558
carlaulloa <carla.ulloa.lp@gmail.com>
2020-04-11 07:49:27 -0500

    feat:base

---
+++ src/main/java/com/app/aforo255/historical/ServucesBankaforo255HistoricalApplication.java
@@ -0,0 +1,13 @@
+package com.app.aforo255.historical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServucesBankaforo255HistoricalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServucesBankaforo255HistoricalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/aforo255/historical/ServucesBankaforo255HistoricalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.aforo255.historical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServucesBankaforo255HistoricalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
